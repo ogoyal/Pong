2d72318fc3def2a879fb4f3b754fc88464edee8e
ogoyal <ojasgoyal@gmail.com>
2023-10-03 00:17:16 -0700

    Add speed to ball

--- Pong/static/js/script.js
+++ Pong/static/js/script.js
@@ -1,14 +1,16 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext("2d");
 let gamePaused = false;
+let hitCounter = 0;
+const MAX_SPEED = 7;
 
 const ball = {
     x: canvas.width / 2,
     y: canvas.height / 2,
     radius: 10,
-    speed: 2,
-    dx: 2,
-    dy: 2,
+    speed: 3,
+    dx: 3,
+    dy: 3,
     color: "white"
 };
 
@@ -17,8 +19,8 @@ const userPaddle = {
     y: canvas.height / 2 - 40,
     width: 10,
     height: 80,
-    dy: 4,
-    color: "white"
+    dy: 8,
+    color: "blue"
 };
 
 const compPaddle = {
@@ -26,8 +28,8 @@ const compPaddle = {
     y: canvas.height / 2 - 40,
     width: 10,
     height: 80,
-    dy: 4,
-    color: "white"
+    dy: 8,
+    color: "red"
 };
 
 let userScore = 0;
@@ -83,7 +85,24 @@ function update() {
     if ((ball.x - ball.radius < userPaddle.x + userPaddle.width && ball.y + ball.radius > userPaddle.y && ball.y - ball.radius < userPaddle.y + userPaddle.height) ||
         (ball.x + ball.radius > compPaddle.x && ball.y + ball.radius > compPaddle.y && ball.y - ball.radius < compPaddle.y + compPaddle.height)) {
         ball.dx = -ball.dx;
+        hitCounter++;
     }
+
+    if (hitCounter >= 3) {  // Change 3 to any number of hits you prefer
+        if (Math.abs(ball.dx) < MAX_SPEED) {
+            ball.dx = (ball.dx > 0) ? ball.dx + 1 : ball.dx - 1;  // Increase speed in the x-direction
+        }
+        if (Math.abs(ball.dy) < MAX_SPEED) {
+            ball.dy = (ball.dy > 0) ? ball.dy + 1 : ball.dy - 1;  // Increase speed in the y-direction
+        }
+        hitCounter = 0;  // Reset the hit counter
+    }    
+
+    // Computer paddle movement (Simple AI)
+    compPaddle.y += (ball.y - (compPaddle.y + compPaddle.height / 2)) * 0.1;
+
+    // Ensure computer paddle doesn't move out of canvas
+    compPaddle.y = Math.max(0, Math.min(canvas.height - compPaddle.height, compPaddle.y));
 
     // Ball out of bounds
     if (ball.x + ball.radius > canvas.width) {
